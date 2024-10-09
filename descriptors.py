115fa313b6b2f950b3379ababfb23b3c60d66487
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2024-10-09 15:48:40 +0200

    Added multiple descriptors

--- descriptors.py
+++ descriptors.py
@@ -1,15 +1,87 @@
 import numpy as np
-from networkit.centrality import Betweenness
-from networkit.distance import APSP, AlgebraicDistance
+from networkit.centrality import Betweenness, DegreeCentrality, Closeness, LocalClusteringCoefficient, PageRank, EigenvectorCentrality
+from networkit.distance import APSP, AlgebraicDistance, Diameter
 from networkit.graph import Graph
-from networkit.linkprediction import JaccardIndex, AdjustedRandIndex, AdamicAdarIndex
-from networkit.sparsification import LocalDegreeScore, LocalSimilarityScore, TriangleEdgeScore
+from networkit.linkprediction import JaccardIndex, AdjustedRandIndex, AdamicAdarIndex, CommonNeighborsIndex, KatzIndex, PreferentialAttachmentIndex
+from networkit.sparsification import LocalDegreeScore, LocalSimilarityScore, TriangleEdgeScore, SCANStructuralSimilarityScore
+from networkit.graphtools import GraphTools
 
 
 def get_triangles(G: Graph):
     edge_triangles = TriangleEdgeScore(G)
     edge_triangles.run()
     return edge_triangles.scores()
+
+
+def calculate_degree_centrality(graph: Graph) -> np.array:
+    dc = DegreeCentrality(graph).run().scores()
+    scores = np.array(dc, dtype=np.float32)
+    return scores
+
+
+def calculate_closeness(graph: Graph) -> np.array:
+    c = Closeness(graph, normalized=True, checkConnectedness=False).run().scores()
+    scores = np.array(c, dtype=np.float32)
+    return scores
+
+
+def calculate_local_clustering_coefficient(graph: Graph) -> np.array:
+    lcc = LocalClusteringCoefficient(graph).run().scores()
+    scores = np.array(lcc, dtype=np.float32)
+    return scores
+
+
+def calculate_pagerank(graph: Graph) -> np.array:
+    pr = PageRank(graph).run().scores()
+    scores = np.array(pr, dtype=np.float32)
+    return scores
+
+
+def calculate_eigenvector_centrality(graph: Graph) -> np.array:
+    ec = EigenvectorCentrality(graph).run().scores()
+    scores = np.array(ec, dtype=np.float32)
+    return scores
+
+
+def calculate_algebraic_distance(graph: Graph) -> np.array:
+    ad = AlgebraicDistance(graph).getEdgeScores()
+    scores = np.array(ad, dtype=np.float32)
+    return scores
+
+
+def calculate_diameter(graph: Graph) -> np.array:
+    pr = Diameter(graph).run()
+    return pr
+
+
+def calculate_common_neighbor_index(graph: Graph) -> np.array:
+    scores = [CommonNeighborsIndex(graph).run(u, v) for u, v in graph.iterEdges()]
+    scores = np.array(scores, dtype=np.float32)
+    return scores
+
+
+def calculate_katz_index(graph: Graph) -> np.array:
+    scores = [KatzIndex(graph).run(u, v) for u, v in graph.iterEdges()]
+    scores = np.array(scores, dtype=np.float32)
+    return scores
+
+
+def calculate_preferential_attachment_index(graph: Graph) -> np.array:
+    scores = [PreferentialAttachmentIndex(graph).run(u, v) for u, v in graph.iterEdges()]
+    scores = np.array(scores, dtype=np.float32)
+    return scores
+
+
+def calculate_scan(graph: Graph) -> np.array:
+    pr = SCANStructuralSimilarityScore(graph, get_triangles(graph)).run().scores()
+    scores = np.array(pr, dtype=np.float32)
+    return scores
+
+
+def calculate_density(graph: Graph) -> np.array:
+    gt = GraphTools()
+    d = gt.density(graph)
+    return d
 
 
 def calculate_shortest_paths(graph: Graph) -> np.array:
